81f27de679dc74d21c57398f2fb7f1be628f78d8
Aljaž Šmaljcelj <aljaz.smaljcelj7@gmail.com>
2023-01-10 00:55:02 +0100

    [#20] implementation of all the tasks done already on other microservices
    - added correct namespace, env variables and liveness, readdines probes to deployment file
    - implemented endpoints for changing readiness and liveness
    - added logging

---
+++ src/main/java/com/rso/microservice/filter/LoggingFilter.java
@@ -0,0 +1,39 @@
+package com.rso.microservice.filter;
+
+import com.rso.microservice.util.MDCUtil;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+
+@Component
+@Order(1)
+public class LoggingFilter implements Filter {
+
+	final BuildProperties buildProperties;
+
+	public LoggingFilter(BuildProperties buildProperties) {
+		this.buildProperties = buildProperties;
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) servletRequest;
+
+		String requestId = req.getHeader("X-Request-Id");
+		if (requestId == null || requestId.isBlank())
+			requestId = UUID.randomUUID().toString();
+
+		MDCUtil.put(MDCUtil.MDCUtilKey.MICROSERVICE_NAME, "Administration");
+		MDCUtil.put(MDCUtil.MDCUtilKey.REQUEST_ID, requestId);
+		MDCUtil.put(MDCUtil.MDCUtilKey.MICROSERVICE_VERSION, buildProperties.getVersion());
+		filterChain.doFilter(servletRequest, servletResponse);
+		MDCUtil.clear();
+	}
+
+}
